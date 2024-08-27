463b3534f63dc989adf9a52f068ba38990092166
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-08-27 08:37:42 +0100

    documentation of external interfaces

--- algotrade4j-market-data/src/main/java/dev/jwtly10/marketdata/common/ClientCallback.java
+++ algotrade4j-market-data/src/main/java/dev/jwtly10/marketdata/common/ClientCallback.java
@@ -2,10 +2,27 @@ package dev.jwtly10.marketdata.common;
 
 import dev.jwtly10.core.model.Bar;
 
+/**
+ * ClientCallback Is used to trigger certain events on data, on error and on data complete
+ */
 public interface ClientCallback {
+    /**
+     * Trigger logic on new bar data
+     *
+     * @param bar the bar from the data source
+     * @return true is there is candle data/should continue running
+     */
     boolean onCandle(Bar bar);
 
+    /**
+     * Trigger an on error event
+     *
+     * @param exception the exception that was thrown by internal logic
+     */
     void onError(Exception exception);
 
+    /**
+     * Trigger data complete event
+     */
     void onComplete();
 }
