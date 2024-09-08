6a4229f2297c82e9ae26513f2fd4de6bc1a7ea88
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-09-08 17:54:45 +0100

    new module - live service

---
+++ algotrade4j-liveservice/src/main/java/dev/jwtly10/liveservice/Algotrade4jLiveserviceApplication.java
@@ -0,0 +1,13 @@
+package dev.jwtly10.liveservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algotrade4jLiveserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Algotrade4jLiveserviceApplication.class, args);
+    }
+
+}
