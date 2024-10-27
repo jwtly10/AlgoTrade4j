cb1774aa3dd7d39d87594512d09d883f7fd8f01c
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-10-27 22:44:42 +0000

    layout tweaks

--- algotrade4j-frontend/src/views/main/HomeView.jsx
+++ algotrade4j-frontend/src/views/main/HomeView.jsx
@@ -146,7 +146,7 @@ const HomeView = () => {
     }, [])
 
     return (
-        <div className="container mx-auto p-6">
+        <div className="container mx-auto px-4 py-4 sm:py-8">
             <h1 className="text-3xl font-bold mb-6">Welcome to AlgoTrade4j</h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
