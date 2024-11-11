aa55f13d2388fd4a8b9ae2fd972bb1f4f13f2282
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-11-11 08:58:14 +0000

    Add broker accounts

--- vite-template-javascript/src/pages/dashboard/service/trading/list.jsx
+++ vite-template-javascript/src/pages/dashboard/service/trading/list.jsx
@@ -10,27 +10,20 @@ import { config } from '@/config';
 import { liveClient } from '@/lib/api/auth/live-client';
 import { logger } from '@/lib/default-logger';
 import { StrategyCard } from '@/components/dashboard/service/trading/live-strategy-card';
+import { brokerClient } from '@/lib/api/auth/broker-client';
+import BrokerAccountCard from '@/components/dashboard/service/trading/broker-card';
 
 const metadata = { title: `Live Strategies | Dashboard | ${config.site.name}` };
 
 export function Page() {
   const intervalRef = React.useRef(null);
   const [liveStrategies, setLiveStrategies] = React.useState([]);
+  const [brokerAccounts, setBrokerAccounts] = React.useState([]);
   const [idToggling, setIdToggling] = React.useState(null);
-
-  const handleToggle = async (strategy) => {
-    try {
-      setIdToggling(strategy.id);
-      await liveClient.toggleStrategy(strategy.id);
-      fetchLiveStrategies();
-      setIdToggling(null);
-    } catch (error) {
-      logger.error('Error toggling live strategy', error);
-    }
-  };
 
   React.useEffect(() => {
     fetchLiveStrategies();
+    fetchBrokerAccounts();
 
     intervalRef.current = setInterval(() => {
       fetchLiveStrategies();
@@ -50,6 +43,30 @@ export function Page() {
     } catch (error) {
       logger.error('Error getting live strategies from db', error);
     }
+  };
+
+  const fetchBrokerAccounts = async () => {
+    try {
+      const res = await brokerClient.getBrokerAccounts();
+      setBrokerAccounts(res);
+    } catch (error) {
+      logger.error('Error getting broker accounts', error);
+    }
+  };
+
+  const handleToggle = async (strategy) => {
+    try {
+      setIdToggling(strategy.id);
+      await liveClient.toggleStrategy(strategy.id);
+      fetchLiveStrategies();
+      setIdToggling(null);
+    } catch (error) {
+      logger.error('Error toggling live strategy', error);
+    }
+  };
+
+  const handleEditBrokerAccount = (account) => {
+    logger.debug('Editing broker account', account);
   };
 
   return (
@@ -80,6 +97,31 @@ export function Page() {
             {liveStrategies.map((strategy) => (
               <Grid size={{ xs: 12, md: 6, lg: 6 }} key={strategy.id}>
                 <StrategyCard strategy={strategy} handleToggle={handleToggle} toggling={idToggling === strategy.id} />
+              </Grid>
+            ))}
+          </Grid>
+          <Stack direction={{ xs: 'row', sm: 'row' }} spacing={3} sx={{ alignItems: 'flex-start', mt: 6 }}>
+            <Box sx={{ flex: '1 1 auto' }}>
+              <Typography variant="h4">Broker Accounts</Typography>
+            </Box>
+            <div>
+              <Button startIcon={<PlusIcon />} variant="contained">
+                New Broker Account
+              </Button>
+            </div>
+          </Stack>
+          <Grid container spacing={2}>
+            {brokerAccounts.map((account) => (
+              <Grid
+                size={{
+                  xs: 12,
+                  sm: 6,
+                  md: 4,
+                  lg: 3,
+                }}
+                key={account.id}
+              >
+                <BrokerAccountCard account={account} onEdit={handleEditBrokerAccount} />
               </Grid>
             ))}
           </Grid>
