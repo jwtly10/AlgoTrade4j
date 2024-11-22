f9cd06af7ced1c07f54b8e080a1fc892c13dd659
Josh Watley <watleyjosh@gmail.com>
2024-11-22 22:27:35 +0000

    remove old frontend project

--- algotrade4j-frontend/src/logger.js
+++
@@ -1,8 +0,0 @@
-import log from 'loglevel';
-
-const isDev = import.meta.env.VITE_NODE_ENV === 'dev';
-const isDebugEnabled = import.meta.env.VITE_ENABLE_DEBUG_LOGS === 'true';
-
-log.setLevel(isDev || isDebugEnabled ? 'debug' : 'info');
-
-export default log;
